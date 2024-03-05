3710e552de2670ec571bc40295e9b5a3f3857f46
Santiago Giraldo <69825324+sgiraldo2@users.noreply.github.com>
2024-03-04 22:04:41 -0600

    Add bootstrap and vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: ['bootstrap/dist/css/bootstrap.css', 'bootstrap-icons/font/bootstrap-icons.css'],
+  plugins: [
+    { src: '~/plugins/vercel.js', mode: 'client' },
+    { src: '~/plugins/bootstrap.js', mode: 'client' },
+  ],
+  devServer: {
+    port: 3020,
+  },
+  modules: ['@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'dark', // default value of $colorMode.preference
+    // fallback: 'dark', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    dataValue: 'bs-theme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
+  nitro: {
+    preset: 'vercel-edge',
+  },
+  devtools: { enabled: true },
+});
