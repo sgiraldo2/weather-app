0809b35510a3a7f07970e0f8660839ec19915c10
Santiago Giraldo <69825324+sgiraldo2@users.noreply.github.com>
2024-03-28 13:44:58 -0500

    Add maptiler to map page

--- pages/map.vue
+++ pages/map.vue
@@ -1,10 +1,12 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
   <div>
     <div class="container mt-3">
       <h1 class="text-center">Map Page</h1>
-      <p class="text-center">Map this project Page under construction...</p>
+      <TestMap />
     </div>
   </div>
 </template>
